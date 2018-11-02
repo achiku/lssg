7e5ffd0be407c60be4feb63a863360a11dc7a97f
Akira Chiku <akira.chiku@gmail.com>
2018-11-02 17:00:44 +0900

    Add struct for security group

--- parse_sg.go
+++ parse_sg.go
@@ -4,20 +4,43 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/hcl"
 	"github.com/pkg/errors"
 )
 
-func parseSG(f io.Reader) error {
+// SecurityGroup aws security group
+type SecurityGroup struct {
+	Name        string `hcl:"name"`
+	Description string `hcl:"description"`
+	Egress      struct {
+		CIDR     []string `hcl:"cidr_blocks"`
+		FromPort string   `hcl:"from_port"`
+		ToPort   string   `hcl:"to_port"`
+		Protocol string   `hcl:"protocol"`
+	} `hcl:"egress"`
+	Inress struct {
+		CIDR     []string `hcl:"cidr_blocks"`
+		FromPort string   `hcl:"from_port"`
+		ToPort   string   `hcl:"to_port"`
+		Protocol string   `hcl:"protocol"`
+	} `hcl:"ingress"`
+	VpcID string `hcl:"vpc_id"`
+}
+
+// SecurityGroupResource resource
+type SecurityGroupResource struct {
+	Resources map[string]map[string]*SecurityGroup `hcl:"resource,expand"`
+}
+
+// ParseSG parse security groups
+func ParseSG(f io.Reader) (*SecurityGroupResource, error) {
 	buf, err := ioutil.ReadAll(f)
 	if err != nil {
-		return errors.Wrap(err, "failed to read")
+		return nil, errors.Wrap(err, "failed to read")
 	}
-	as, err := hcl.ParseBytes(buf)
-	if err != nil {
-		return err
+	var sgs SecurityGroupResource
+	if err := hcl.Decode(&sgs, string(buf)); err != nil {
+		return nil, err
 	}
-	spew.Dump(as)
-	return nil
+	return &sgs, nil
 }
--- parse_sg_test.go
+++ parse_sg_test.go
@@ -1,19 +1,86 @@
 package main
 
 import (
-	"os"
+	"strings"
 	"testing"
+
+	"github.com/hashicorp/hcl"
 )
 
-func TestParseSG(t *testing.T) {
-	p := os.Getenv("LSSG_TEST_TF")
-	f, err := os.Open(p)
-	if err != nil {
+var simpleconif = `
+directory "config" {
+    source_dir = "/etc/eventstore"
+    dest_prefix = "escluster/config"
+    exclude = []
+    pre_backup_script = "before_backup.sh"
+    post_backup_script = "after_backup.sh"
+    pre_restore_script = "before_restore.sh"
+    post_restore_script = "after_restore.sh"
+}
+`
+
+var simplesg = `
+resource "aws_security_group" "bastion-1" {
+  description = "bastion-1 security group"
+  name = "sg-bastion-1"
+  egress = {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "0"
+    protocol = "-1"
+    to_port = "0"
+  }
+  ingress = {
+    cidr_blocks = [
+        "172.12.99.82/32",
+    ]
+    protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+  }
+  tags {
+    Name = "sg-bastion-1"
+  }
+  vpc_id = "xxxxxxxxx"
+}
+
+resource "aws_security_group" "bastion-2" {
+  description = "bastion-2 security group"
+  name = "sg-bastion-2"
+  egress = {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "0"
+    protocol = "-1"
+    to_port = "0"
+  }
+  ingress = {
+    cidr_blocks = [
+        "172.12.99.11/32",
+    ]
+    protocol = "tcp"
+    from_port = "22"
+    to_port = "22"
+  }
+  tags {
+    Name = "sg-bastion-2"
+  }
+  vpc_id = "xxxxxxxxx"
+}
+`
+
+func TestDecodeSimpleSG(t *testing.T) {
+	var f SecurityGroupResource
+	if err := hcl.Decode(&f, simplesg); err != nil {
 		t.Fatal(err)
 	}
-	defer f.Close()
+	for k, v := range f.Resources {
+		for _, sg := range v {
+			t.Logf("id=%s: %+v", k, sg)
+		}
+	}
+}
 
-	if err := parseSG(f); err != nil {
+func TestParseSG(t *testing.T) {
+	if err := parseSG(strings.NewReader(simplesg)); err != nil {
 		t.Fatal(err)
 	}
 }
